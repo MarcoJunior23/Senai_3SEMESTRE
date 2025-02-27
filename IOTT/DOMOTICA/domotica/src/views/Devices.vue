b67dd7694853ed91ad015e8379dbf5aa53fd1b00
Marco <marcos97795@gmail.com>
2025-02-27 20:15:06 -0300

    att

--- IOTT/DOMOTICA/domotica/src/views/Devices.vue
+++ IOTT/DOMOTICA/domotica/src/views/Devices.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
+    import { Environment } from "@/models/devices";
+    import { reactive, ref   } from 'vue';
 
-  
+    const Environments = reactive()
+
+    const sala: Environment = {
+        name: 'Sala de Estar',
+        devices: [
+{
+    name: 'Ar condicionado',
+    state: false,
+    color: '#3266a8'
+},
+{
+    name: 'Smart TV',
+    state: true,
+    color: '#3266a8'
+},
+{
+    name: 'Iluminação',
+    state: false,
+    color: '#3266a8'
+},
+     
+                 ]
+    }
 </script>
 
 <template>
-
+    <h1>Devices Page!</h1>
+    {{ sala }}
 </template>
 
-<style scoped lang="scss" >
+<style scoped lang="scss">
+
 </style>
---
+++ IOTT/DOMOTICA/domotica/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1> Page Not Found 😒</h1>
+    <img src="https://agentestudio.com/uploads/post/image/69/main_how_to_design_404_page.png"></img>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
