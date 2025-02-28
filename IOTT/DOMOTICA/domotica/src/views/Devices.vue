692c0d418b5cc5a7231d3d89e144e573188cfa96
Marco <marcos97795@gmail.com>
2025-02-27 21:39:26 -0300

    stt

--- IOTT/DOMOTICA/domotica/src/views/Devices.vue
+++ IOTT/DOMOTICA/domotica/src/views/Devices.vue
@@ -2,34 +2,37 @@
     import { Environment } from "@/models/devices";
     import { reactive, ref   } from 'vue';
 
-    const Environments = reactive()
 
-    const sala: Environment = {
-        name: 'Sala de Estar',
-        devices: [
-{
-    name: 'Ar condicionado',
-    state: false,
-    color: '#3266a8'
-},
-{
-    name: 'Smart TV',
-    state: true,
-    color: '#3266a8'
-},
-{
-    name: 'Iluminação',
-    state: false,
-    color: '#3266a8'
-},
-     
-                 ]
-    }
+    const environments: Array<Environment> = reactive([]);
+    
+    
+    const ar: Device = reactive(new Device());
+    ar.name = 'Ar condicionado';
+    ar.color = '#ba1307'
+
+    const tv: Device = reactive(new Device());
+    tv.name = 'Smart TV';
+    tv.color = '#f2f244'
+    tv.state = true;
+
+    const iluminacao: Device = reactive(new Device());
+    iluminacao.name = 'Iluminação';
+    iluminacao.color = '#00eddd'
+
+    const sala: Environment = reactive(new Environment());
+    sala.name = "Sala";
+    sala.devices = [ar, tv,iluminacao];
+
+    environments.push(sala);
 </script>
 
-<template>
-    <h1>Devices Page!</h1>
-    {{ sala }}
+<template>  
+<main class="flex flex-column">
+    <h1>Devices Page</h1>
+  <section>
+
+  </section>
+</main>
 </template>
 
 <style scoped lang="scss">
